7fc7194fa269af814f418488c4b3303a469a901c
Andres Urrego <ingenieroandresangel@gmail.com>
2020-04-30 16:17:23 -0400

    Create createtables.sql

---
+++ src/snowflakeddl/createtables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE KINESIS_SENSORS_MICROBATCH  (
+    sensorid int,
+    currenttemperature int,
+    status string
+); 
+
+CREATE TABLE KINESIS_SENSORS_STREAMING  (
+    sensorid int,
+    currenttemperature int,
+    status string
+); 
